b32d0256decb13416e992361743c1572598f39d1
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-25 15:18:59 -0400

    add login and logout functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -26,7 +27,7 @@ router.get('/', (req, res) => {
         }
       ]
     })
-      .then(dbPostData => {
+    .then(dbPostData => {
         // pass a single post object into the homepage template
         // console.log(dbPostData[0]);
         const posts = dbPostData.map(post => post.get({ plain: true }));
@@ -35,7 +36,16 @@ router.get('/', (req, res) => {
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
-      });
- });
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
