3c68b1406c34e351530423b24a0c22a637f42db2
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-26 11:11:41 -0400

    refactor code using partials

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
